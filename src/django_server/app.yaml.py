1a64bc33babd970bf4638a764309f2c6763f0d98
Stanislav Bondarenko <stassss2009@gmail.com>
2020-11-15 17:23:51 +0200

    Update

--- src/django_server/app.yaml.py
+++ src/django_server/app.yaml.py
@@ -1,12 +1,9 @@
 import os
 
 django_env = os.getenv('DJANGO_ENV', 'development')
-if django_env != "master":
-    service_name = f"django-{django_env}"
-else:
-    service_name = "default" 
+service_name = "django-{}".format(django_env) if django_env != "master" else "default" 
 
-string = f"runtime: python38\n\nservice: service_name\n\nenv_variables:\n  DJANGO_ENV: \"{django_env}\"\n\nhandlers:\n  - url: /.*\n    script: auto"
+string = "runtime: python38\n\nservice: {}\n\nenv_variables:\n  DJANGO_ENV: \"{}\"\n\nhandlers:\n  - url: /.*\n    script: auto".format(service_name, django_env)
 
 with open("./app.yaml", "w") as file:
     file.write(string)
