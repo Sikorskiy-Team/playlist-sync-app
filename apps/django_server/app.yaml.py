3e3a4b7596f3404f1ddf9d9e5b5515d4263c7c2a
Stanislav Bondarenko <stassss2009@gmail.com>
2020-11-20 02:27:32 +0200

    CI update

--- apps/django_server/app.yaml.py
+++ apps/django_server/app.yaml.py
@@ -1,7 +1,7 @@
 import os
 
 django_env = os.getenv('DJANGO_ENV', 'development')
-service_name = "django-{}".format(django_env) if django_env != "master" else "default" 
+service_name = "{}".format(django_env) if django_env != "master" else "default" 
 
 string = "runtime: python38\n\nservice: {}\n\nenv_variables:\n  DJANGO_ENV: \"{}\"\n\nhandlers:\n  - url: /.*\n    script: auto".format(service_name, django_env)
 
