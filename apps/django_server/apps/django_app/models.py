cf12c71b325273957d005899935b9d8aba1cf67a
demon <worthdyingfor8@gmail.com>
2020-12-23 22:05:02 +0200

    Auth0 fix

--- apps/django_server/apps/django_app/models.py
+++ apps/django_server/apps/django_app/models.py
@@ -5,6 +5,7 @@ class User(models.Model):
     _id = models.ObjectIdField()
     name = models.CharField(max_length=100)
     email = models.TextField(max_length=100)
+    auth0_token = models.SlugField(max_length = 1000)
 
     class Meta:
         ordering = ['name']
